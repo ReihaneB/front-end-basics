d7d913b4564516d94995f10a322677d73f541e5e
Reihane Blosenhauer <reihaneb@liome.fr>
2024-02-19 19:24:56 +0100

    Implementation of useFocusable

---
+++ src/hooks/useFocusable/useFocusable.stories.tsx
@@ -0,0 +1,101 @@
+import { useRef } from 'react';
+import { userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+import { useFocusable } from './useFocusable';
+
+import styles from './useFocusable.stories.module.css';
+
+export default {
+  title: 'Hooks/UseFocusable',
+  component: useFocusable,
+  argTypes: {
+    autoFocus: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+};
+
+function FocusableComponent({
+  autoFocus = false,
+  buttonTestId = 'focus-button',
+  focusableTestId = 'focusable',
+}) {
+  const ref = useRef<HTMLElement | null>(null);
+
+  const { ref: elementRef, setFocus } = useFocusable(ref, { autoFocus });
+
+  return (
+    <div>
+      <div
+        className={styles.focusable}
+        role="button"
+        tabIndex={0}
+        ref={elementRef}
+        data-testid={focusableTestId}
+      >
+        Click the button to focus it. This element is
+      </div>
+      <button
+        type="button"
+        onClick={setFocus}
+        data-testid={buttonTestId}
+      >
+        Click me to focus
+      </button>
+    </div>
+  );
+}
+
+export function Playground({...args}) {
+  return <FocusableComponent {...args} />;
+}
+
+Playground.args = {
+  autoFocus: false,
+};
+
+export function AutoFocus() {
+  return <FocusableComponent autoFocus />;
+}
+
+export function ClickToFocus() {
+  return <FocusableComponent />;
+}
+
+ClickToFocus.play = async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+  const canvas = within(canvasElement);
+
+  await userEvent.click(canvas.getByTestId('focus-button'));
+};
+
+export function FocusOnTab() {
+  return (
+    <FocusableComponent />
+  );
+}
+
+FocusOnTab.play = async () => {
+  await userEvent.tab();
+};
+
+export function FocusAssociated() {
+  return (
+    <div>
+      <FocusableComponent buttonTestId="focus-button-1" focusableTestId="focusable-1" />
+      <FocusableComponent buttonTestId="focus-button-2" focusableTestId="focusable-2" />
+    </div>
+  );
+}
+
+FocusAssociated.storyName = 'Focus the correct element associated with its ref on click';
+
+FocusAssociated.play = async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+  const canvas = within(canvasElement);
+
+  await userEvent.click(canvas.getByTestId('focus-button-2'));
+
+  await expect(canvas.getByTestId('focusable-2')).toHaveFocus();
+};
