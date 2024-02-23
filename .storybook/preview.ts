fd7916d457804fbead606322364f102a602e8673
Reihane Blosenhauer <reihaneb@liome.fr>
2024-02-24 00:02:52 +0100

    Implementation of global styles in Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,8 @@ import type { Preview } from "@storybook/react";
 
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
+import '@/styles/globals.css';
+
 const preview: Preview = {
   parameters: {
     viewport: {
