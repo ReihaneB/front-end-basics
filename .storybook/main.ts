889b0086aca373e40431b94e744bf589b9494160
Reihane Blosenhauer <reihaneb@liome.fr>
2024-02-25 21:38:05 +0100

    Remove unused SVGR dependencies and update configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -18,33 +18,7 @@ const config: StorybookConfig = {
     options: {},
   },
   webpackFinal: (config: any) => {
-    // this modifies the existing image rule to exclude .svg files
-    // since we want to handle those files with @svgr/webpack
-    const svgRules = config.module.rules.filter((rule) =>
-      rule.test?.test('.svg')
-    );
-    svgRules.forEach((rule) => {
-      rule.exclude = /\.svg$/;
-    });
-
-    // configure .svg files to be loaded with @svgr/webpack and as assets
-    config.module.rules.push(
-      {
-        test: /\.svg$/i,
-        type: 'asset',
-        resourceQuery: /url/, // *.svg?url
-      },
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: {
-          not: [/url/],
-        },
-        // exclude react component if *.svg?url
-        use: ['@svgr/webpack'],
-      }
-    );
-
+    // Add support for TypeScript path mapping
     config.resolve.plugins = [
       ...(config.resolve.plugins || []),
       new TsconfigPathsPlugin({
