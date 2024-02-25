fbe98adb271553c63fc8ba46b1692b8fc82f11cd
Reihane Blosenhauer <reihaneb@liome.fr>
2024-02-25 21:38:05 +0100

    Remove unused SVGR dependencies and update configuration

--- src/testUtils/svgMock.tsx
+++
@@ -1,8 +0,0 @@
-import { forwardRef } from 'react';
-
-const SvgrMock = forwardRef<HTMLInputElement>(({ ...rest }, ref) => (
-  <div ref={ref} {...rest} />
-));
-
-export const ReactComponent = SvgrMock;
-export default SvgrMock;
